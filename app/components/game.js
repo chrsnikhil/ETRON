51e7355c0984e9c7a7337b84d30dac0657590aff
Nikhil <chrsnikhil@gmail.com>
2025-02-06 15:43:53 +0530

    Initial commit

---
+++ app/components/game.js
@@ -0,0 +1,45 @@
+"use client";
+import { Scene } from 'phaser'; 
+import React, { useEffect } from 'react';
+
+class MainScene extends Scene { 
+  constructor() {
+    super({ key: 'MainScene' });
+    // ... existing code ...
+  }
+    preload() {
+        // Load assets here
+        this.load.image('logo', '/public/vercel.svg');
+    }
+
+    create() {
+        // Add assets to the scene
+        this.add.image(400, 300, 'logo');
+    }
+
+    update() {
+        // Game logic goes here
+    }
+}
+
+const PhaserGame = () => {
+  useEffect(() => {
+    const config = {
+      type: Phaser.AUTO,
+      width: 800,
+      height: 600,
+      parent: 'game-container',
+      scene: [MainScene],
+    };
+
+    const game = new Phaser.Game(config);
+
+    return () => {
+      game.destroy(true);
+    };
+  }, []);
+
+  return <div id="game-container"></div>;
+};
+
+export default PhaserGame;
